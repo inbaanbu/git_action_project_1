1580f0b563a79d0e95f91e7bbe32271a4c5a2ea8
inbaanbu <inbarasan19981230@gmail.com>
2024-01-20 15:44:51 +0530

    bug fix

--- src/main/java/git_action_project_1/aws/GitActionProject1Application.java
+++ src/main/java/git_action_project_1/aws/GitActionProject1Application.java
@@ -2,12 +2,18 @@ package git_action_project_1.aws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class GitActionProject1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitActionProject1Application.class, args);
+	}
+	
+	@GetMapping("/user/details")
+	public String user() {
+		return "hii inba code is working";
 	}
 
 }
